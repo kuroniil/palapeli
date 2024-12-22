e27f116f50b5dcddb58afc3b16530620270ce9c6
Niilo Kuronen <niilo.kuronen@helsinki.fi>
2024-12-22 13:05:46 +0200

    new score, get all and get by mode works

--- server/controllers/scores.js
+++ server/controllers/scores.js
@@ -11,44 +11,50 @@ router.get('/', async (request, response) => {
 
 router.get('/:mode', async (request, response) => {
     const mode = request.params.mode
+    const scoresByMode = await Score.findAll({
+        where: {"mode": mode}
+    })
     response.json(scoresByMode)
   })
 
-router.get('/:mode/:id', (request, response) => {
-    const mode = request.params.mode
-    const id = request.params.id
-    const scoresByMode = scores.filter(score => score.mode === mode)
-    const score = scoresByMode.find(score => score.id === id)
-    if (score) {
+router.post('/', async (request, response) => {
+    try {
+        const score = await Score.create(request.body)
         response.json(score)
-    } else {
-        response.status(404).end()
-    }
-  })
-
-router.get('/:id', (request, response) =>  {
-    const id = request.params.id
-    const score = scores.find(score => score.id === id)
-    if (score) {
-        response.json(score)
-    } else {
-        response.status(404).end()
+    } catch(error) {
+        return response.status(400).json({ error })
     }
 })
 
-router.post('/', (request, response) => {
-    const id = Math.max(scores.map(score => parseInt(score.id))) + 1
-    const score = request.body
-    score.id = id
-    response.json(score)
-})
 
-router.put('/:id', (request, response) => {
-    const id = request.params.id
-    const score = request.body
-    scores.filter(scores => scores.id !== id)
-    scores.concat(scores, score)
-    response.json(score)
-})
+//router.get('/:mode/:id', (request, response) => {
+//    const mode = request.params.mode
+//    const id = request.params.id
+//    const scoresByMode = scores.filter(score => score.mode === mode)
+//    const score = scoresByMode.find(score => score.id === id)
+//    if (score) {
+//        response.json(score)
+//    } else {
+//        response.status(404).end()
+//    }
+//  })
+//
+//router.get('/:id', (request, response) =>  {
+//    const id = request.params.id
+//    const score = scores.find(score => score.id === id)
+//    if (score) {
+//        response.json(score)
+//    } else {
+//        response.status(404).end()
+//    }
+//})
+//
+//router.put('/:id', (request, response) => {
+//    const id = request.params.id
+//    const score = request.body
+//    scores.filter(scores => scores.id !== id)
+//    scores.concat(scores, score)
+//    response.json(score)
+//})
 
 module.exports = router
